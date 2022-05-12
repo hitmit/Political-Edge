e6bd5ca91d0346e4083897e6cfd084c89c5942cf
Hitesh <hiteshmittal1990@gmail.com>
2022-05-12 08:17:41 +0000

    fix upser profjec

--- app/Models/UserProject.php
+++ app/Models/UserProject.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserProject extends Model
 {
     use HasFactory;
+    protected $table = 'assign_projects';
     protected $fillable = ['project_id','user_id'];
     public $timestamps = false;
 }
