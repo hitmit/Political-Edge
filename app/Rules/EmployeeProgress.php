2a59660ec3f32c91a8c33c1a9e3aa8728c5ac5e8
surajkashyap <surajkashyap9911@gmail.com>
2022-05-18 15:46:40 +0530

    correct spelling of check

--- app/Rules/EmployeeProgress.php
+++ app/Rules/EmployeeProgress.php
@@ -46,6 +46,6 @@ class EmployeeProgress implements Rule
      */
     public function message()
     {
-        return 'Your progress couldn\'t be saved. Please chek your total progress.';
+        return 'Your progress couldn\'t be saved. Please check your total progress.';
     }
 }
